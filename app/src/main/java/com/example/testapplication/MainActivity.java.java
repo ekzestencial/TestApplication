197ee509d525cf11aae66130bd49ff093d643c08
Alexander Slesar <ekzestencial@gmail.com>
2017-05-21 00:49:12 +0300

    Add session save

---
+++ app/src/main/java/com/example/testapplication/MainActivity.java.java
@@ -0,0 +1,84 @@
+package com.example.testapplication;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button myBtnRegister;
+    Button myBtnSignUp;
+    EditText emailText;
+    EditText passwordText;
+    SharedPreferences sPref;
+    final String SAVED_TEXT = "saved_text";
+    final String NO_TEXT = "no_text";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (existsSession().equals(NO_TEXT)) {
+            setContentView(R.layout.activity_main);
+
+            myBtnRegister = (Button) findViewById(R.id.register_id);
+            myBtnSignUp = (Button) findViewById(R.id.signUp_Id);
+            emailText = (EditText) findViewById(R.id.Email_id);
+            passwordText = (EditText) findViewById(R.id.Password_id);
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            myBtnRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Second_activity.class);
+                    startActivity(intent);
+                }
+            });
+            myBtnSignUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String strEmail = emailText.getText().toString();
+                    String strPassword = passwordText.getText().toString();
+                    if (SingletonSession.Instance().enterCheckPassword(strEmail, strPassword)) {
+                        Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                        startActivity(intent);
+                    } else {
+                        builder.setTitle("Alert")
+                                .setMessage("Invalid user or password")
+                                .setCancelable(false)
+                                .setNegativeButton("Ok",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                dialog.cancel();
+                                            }
+                                        });
+                        AlertDialog alert = builder.create();
+                        System.out.println("False");
+                        alert.show();
+                    }
+                }
+            });
+
+
+        } else
+
+        {
+            setContentView(R.layout.activity_third);
+        }
+    }
+
+
+    private String existsSession()
+
+    {
+        sPref = getSharedPreferences("SaveSession", MODE_PRIVATE);
+
+        return sPref.getString(SAVED_TEXT, NO_TEXT);
+
+    }
+
+
+}
